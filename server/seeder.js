7f35bac6e3c9e6b3088de8911a0b57aa8e68a8fe
Luong <ctpluong@gmail.com>
2026-05-13 20:05:24 +0700

    complete app

--- server/seeder.js
+++ server/seeder.js
@@ -1,82 +1,83 @@
-import dotenv from 'dotenv';
-import colors from 'colors';
-import bcrypt from 'bcryptjs';
-import users from './data/users.js';
-import products from './data/products.js';
-import discounts from './data/discounts.js'; // Import discount data
-import User from './models/userModel.js';
-import Product from './models/productModel.js';
-import Order from './models/orderModel.js';
-import Discount from './models/discountModel.js'; // Import the Discount model
-import connectDB from './config/db.js';
+import dotenv from 'dotenv'
+import colors from 'colors'
+import bcrypt from 'bcryptjs'
 
-dotenv.config();
+import users from './data/users.js'
+import products from './data/products.js'
+import discounts from './data/discounts.js'
 
-connectDB();
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import Discount from './models/discountModel.js'
+
+import connectDB from './config/db.js'
+
+dotenv.config()
+connectDB()
 
 const importData = async () => {
   try {
-    // Delete existing data
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
-    await Discount.deleteMany(); // Delete existing discounts
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+    await Discount.deleteMany()
 
-    // Insert discounts into the database
-    const createdDiscounts = await Discount.insertMany(discounts);
+    // =========================
+    // 🔥 HASH PASSWORD USERS
+    // =========================
+    const usersWithHashedPassword = users.map((user) => ({
+      ...user,
+      password: bcrypt.hashSync(user.password, 10),
+    }))
 
-    // Get the ObjectIds of the created discounts
-    const discountIds = createdDiscounts.map(discount => discount._id);
+    const createdDiscounts = await Discount.insertMany(discounts)
 
-    // Assign discounts to users
-    users[0].discounts = discountIds;  // Admin User gets all discounts
-    users[1].discounts = [discountIds[1], discountIds[2]];  // Jane gets DISCOUNT20 and SALE10
+    const discountIds = createdDiscounts.map((d) => d._id)
 
-    // Insert users with discounts
-    const createdUsers = await User.insertMany(users);
+    // gán discount cho user trước khi insert
+    usersWithHashedPassword[0].discounts = discountIds
+    usersWithHashedPassword[1].discounts = [
+      discountIds[1],
+      discountIds[2],
+    ]
 
-    // Update the discounts with userId references
-    await Discount.updateMany(
-      { _id: { $in: discountIds } },
-      { $set: { userId: createdUsers[0]._id } } // For example, all discounts can be associated with the admin user, or specific ones
-    );
+    const createdUsers = await User.insertMany(usersWithHashedPassword)
 
-    // Link products to users (admin user for example)
-    const adminUser = createdUsers[0]._id;
+    const adminUser = createdUsers[0]._id
 
-    // Map sample products to the admin user
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
+    const sampleProducts = products.map((p) => ({
+      ...p,
+      user: adminUser,
+    }))
 
-    await Product.insertMany(sampleProducts);
+    await Product.insertMany(sampleProducts)
 
-    console.log('Data Imported!'.green.inverse);
-    process.exit();
+    console.log('DATA IMPORTED SUCCESS'.green.inverse)
+    process.exit()
   } catch (error) {
-    console.error(`${error}`.red.inverse);
-    process.exit(1);
+    console.error(error)
+    process.exit(1)
   }
-};
+}
 
 const destroyData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
-    await Discount.deleteMany(); // Delete discounts too
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+    await Discount.deleteMany()
 
-    console.log('Data Destroyed!'.red.inverse);
-    process.exit();
+    console.log('DATA DESTROYED'.red.inverse)
+    process.exit()
   } catch (error) {
-    console.error(`${error}`.red.inverse);
-    process.exit(1);
+    console.error(error)
+    process.exit(1)
   }
-};
+}
 
-// Check for command line args to destroy data
 if (process.argv[2] === '-d') {
-  destroyData();
+  destroyData()
 } else {
-  importData();
+  importData()
 }
